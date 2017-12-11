8c0fbf274ccb4e917e6702ffb0ff3166c55edbde
Chris McComb <ccmcc2012@gmail.com>
2017-12-10 21:37:01 -0500

    Initial commit.

---
+++ extract_and_structure.py
@@ -0,0 +1,70 @@
+import numpy
+import kagan
+
+# Set constants
+minimum_true_teams = 10
+minimum_indiv_teams = 10
+
+# Load data
+submissions = kagan.load_csv('./meta-kaggle/Submissions.csv', 6)
+competitions = kagan.load_csv('./meta-kaggle/Competitions.csv', 2)
+users = kagan.load_csv('./meta-kaggle/Users.csv', 3)
+teams = kagan.load_csv('./meta-kaggle/Teams.csv', 6)
+team_memberships = kagan.load_csv('./meta-kaggle/TeamMemberships.csv', 3)
+
+# For each team make a userlist and for each competiton a teamlist
+for competitionkey in competitions:
+    competitions[competitionkey]["team_list"] = []
+for teamkey in teams:
+    teams[teamkey]["user_list"] = {}
+
+# Assign teams to members
+memberships = {}
+for membershipkey in team_memberships:
+    the_team = team_memberships[membershipkey]["TeamId"]
+    the_user = team_memberships[membershipkey]["UserId"]
+    teams[the_team]["user_list"][the_user] = 0
+
+# Count submissions per team member
+total_submissions = 0
+error_submissions = 0
+for submissionkey in submissions:
+    the_team = submissions[submissionkey]["TeamId"]
+    the_user = submissions[submissionkey]["SubmittedUserId"]
+    try:
+        teams[the_team]["user_list"][the_user] += 1
+        total_submissions += 1
+    except KeyError:
+        error_submissions += 1
+
+print(total_submissions, error_submissions)
+
+# For each competitions, assign teams to it
+for teamkey in teams:
+    competition_id = teams[teamkey]["CompetitionId"]
+    competitions[competition_id]["team_list"].append(teams[teamkey])
+
+# For each team in each competition, analyze users to it
+good_competitions = []
+for competitionkey in competitions:
+    indiv_teams = 0
+    team_size = []
+    real_teams = 0
+    for team in competitions[competitionkey]["team_list"]:
+        if len(team["user_list"]) == 1:
+            indiv_teams += 1
+        else:
+            real_teams += 1
+            team_size.append(len(team["user_list"]))
+
+    competitions[competitionkey]["indiv_team_count"] = indiv_teams
+    competitions[competitionkey]["real_team_count"] = real_teams
+    competitions[competitionkey]["real_team_sizes"] = team_size
+
+    if real_teams > minimum_true_teams and indiv_teams > minimum_indiv_teams:
+        good_competitions.append(competitions[competitionkey])
+
+numpy.savez('good_competitions.npz', good_competitions=good_competitions)
+
+print(len(good_competitions))
+
---
+++ run_comparison.py
@@ -0,0 +1,144 @@
+import numpy
+import scipy.stats
+import matplotlib.pyplot
+import kagan
+
+p = 0.05
+good_competitions = numpy.load("good_competitions.npz")["good_competitions"]
+
+ibt = 0
+tbi = 0
+ibr = 0
+rbi = 0
+tbr = 0
+rbt = 0
+
+ibte = 0
+tbie = 0
+ibre = 0
+rbie = 0
+tbre = 0
+rbte = 0
+
+ibtet = 0
+tbiet = 0
+ibret = 0
+rbiet = 0
+tbret = 0
+rbtet = 0
+
+real_team_scores_all = []
+indiv_team_scores_all = []
+remixed_team_scores_all = []
+real_team_mefforts_all = []
+indiv_team_mefforts_all = []
+remixed_team_mefforts_all = []
+real_team_tefforts_all = []
+indiv_team_tefforts_all = []
+remixed_team_tefforts_all = []
+
+for competition in good_competitions:
+    real_team_scores = []
+    indiv_team_scores = []
+    real_team_mefforts = []
+    indiv_team_mefforts = []
+    real_team_tefforts = []
+    indiv_team_tefforts = []
+
+
+    # Check directionality
+    diffscore = competition["team_list"][0]["Score"] - competition["team_list"][1]["Score"]
+    diffrank = competition["team_list"][0]["Ranking"] - competition["team_list"][1]["Ranking"]
+    if diffscore*diffrank < 0:
+        m = -1
+    else:
+        m = 1
+
+    # Assess quality of teams and individuals
+    for team in competition["team_list"]:
+
+        effort = 0
+        for userkey in team["user_list"]:
+            effort += team["user_list"][userkey]
+
+        if len(team["user_list"]) == 1:
+            indiv_team_scores.append(m*team["Score"])
+            indiv_team_mefforts.append(effort)
+            indiv_team_tefforts.append(effort)
+        else:
+            real_team_scores.append(m*team["Score"])
+            real_team_mefforts.append(effort/len(team["user_list"]))
+            real_team_tefforts.append(effort)
+
+    # Normalize values according to mean and standard distribution
+    mm = numpy.mean(indiv_team_scores)
+    sm = numpy.std(indiv_team_scores)
+    real_team_scores = [(x-mm)/sm for x in real_team_scores]
+    indiv_team_scores = [(x-mm)/sm for x in indiv_team_scores]
+
+    # Normalize values according to mean and standard distribution
+    mm = numpy.mean(indiv_team_mefforts)
+    # sm = numpy.std(real_team_mefforts + indiv_team_mefforts)
+    real_team_mefforts = [x/mm for x in real_team_mefforts]
+    indiv_team_mefforts = [x/mm for x in indiv_team_mefforts]
+
+    # Normalize values according to mean and standard distribution
+    mm = numpy.mean(indiv_team_tefforts)
+    # sm = numpy.std(real_team_tefforts + indiv_team_tefforts)
+    real_team_tefforts = [x/mm for x in real_team_tefforts]
+    indiv_team_tefforts = [x/mm for x in indiv_team_tefforts]
+
+
+    # Get scores for teams resampled from individuals
+    remixed_team_scores = []
+    remixed_team_mefforts = []
+    remixed_team_tefforts = []
+    for _ in range(int(0.5*len(competition["team_list"]))):
+        # Choose a team size
+        team_size = numpy.random.choice(competition["real_team_sizes"])
+
+        # Get remixed average effort
+        efforts = numpy.random.choice(indiv_team_mefforts, team_size, replace=False)
+        remixed_team_mefforts.append(numpy.mean(efforts))
+        remixed_team_tefforts.append(numpy.sum(efforts))
+
+        # Get remixed scores
+        scores = numpy.random.choice(indiv_team_scores, team_size, replace=False)
+        remixed_team_scores.append(numpy.min(scores))
+
+
+    # Save sores
+    real_team_scores_all += real_team_scores
+    indiv_team_scores_all += indiv_team_scores
+    remixed_team_scores_all += remixed_team_scores
+
+    real_team_mefforts_all += real_team_mefforts
+    indiv_team_mefforts_all += indiv_team_mefforts
+    remixed_team_mefforts_all += remixed_team_mefforts
+
+    real_team_tefforts_all += real_team_tefforts
+    indiv_team_tefforts_all += indiv_team_tefforts
+    remixed_team_tefforts_all += remixed_team_tefforts
+
+total = len(good_competitions)
+
+matplotlib.pyplot.figure()
+
+matplotlib.pyplot.plot(numpy.mean(indiv_team_tefforts_all), -numpy.mean(indiv_team_scores_all), 'rs')
+matplotlib.pyplot.plot(numpy.mean(real_team_tefforts_all), -numpy.mean(real_team_scores_all), 'bo')
+matplotlib.pyplot.plot(numpy.mean(remixed_team_tefforts_all), -numpy.mean(remixed_team_scores_all), 'g^')
+
+delta = 0.1
+kagan.plot_slope(numpy.array([0.9, 1.1]), indiv_team_tefforts_all, indiv_team_scores_all, 'r-')
+kagan.plot_slope(numpy.mean(real_team_tefforts_all)+[-delta, delta], real_team_tefforts_all, real_team_scores_all, 'b-')
+kagan.plot_slope(numpy.mean(remixed_team_tefforts_all)+[-delta, delta], remixed_team_tefforts_all, remixed_team_scores_all, 'g-')
+kagan.plot_slope(numpy.array([1.0, numpy.mean(remixed_team_tefforts_all)]), indiv_team_tefforts_all, indiv_team_scores_all, 'r:')
+kagan.plot_slope(numpy.array([numpy.mean(real_team_tefforts_all)-delta, numpy.mean(remixed_team_tefforts_all)]), real_team_tefforts_all, real_team_scores_all, 'b:')
+
+# Add legend and labels
+matplotlib.pyplot.legend(["Individuals", "True Teams", "Nominal Teams"])
+matplotlib.pyplot.xlabel("Total Submissions (Normalized)")
+matplotlib.pyplot.ylabel("Quality of Best Solution (Normalized)")
+
+# Show results
+matplotlib.pyplot.show()
