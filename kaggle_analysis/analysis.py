947f97dee6ab8d8f8564bd00eb5f7cb7ec7b4457
Chris McComb <ccmcc2012@gmail.com>
2017-12-23 23:24:18 -0500

    Fixing travis.

--- kaggle_analysis/analysis.py
+++ kaggle_analysis/analysis.py
@@ -3,10 +3,9 @@ import typing
 import scipy.stats
 import os
 import matplotlib
-# if os.environ.get('DISPLAY', '') == '':
-#     print('no display found. Using non-interactive Agg backend')
-#     matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot
+matplotlib.pyplot.ioff()
 
 
 def plot_slope(x: numpy.ndarray,
