965c87d6a451dbb94d6eb48329a0166fa6a24b61
Chris McComb <ccmcc2012@gmail.com>
2017-12-24 10:03:52 -0500

    Team size v performance analysis added

--- idetc2018.py
+++ idetc2018.py
@@ -1,4 +1,6 @@
 import kaggle_analysis
+import matplotlib.pyplot
+import pkg_resources
 
 # Define minimum values for an acceptable competition
 minimum_true_teams = 10
@@ -11,9 +13,15 @@ competition_info = kaggle_analysis.preprocessing.extract_good_teams(minimum_true
 kaggle_analysis.analysis.plot_team_size_histogram(competition_info)
 
 # Compare nominal teams to true teams and individuals
-kaggle_analysis.analysis.nominal_teams(competition_info)
+real_team, indiv_team, nominal_team = kaggle_analysis.analysis.get_nominal_teams(competition_info)
+kaggle_analysis.analysis.plot_nominal_teams(real_team, indiv_team, nominal_team)
 
-# TODO: Add analysis of performance as function of true team size
-# TODO: Add analysis fo performance as function of nominal team size
+# Plot performance v team size
+matplotlib.pyplot.figure()
+kaggle_analysis.analysis.plot_performance_v_size(real_team, indiv_team, "real_team.png")
+kaggle_analysis.analysis.plot_nominal_performance_v_size(indiv_team, 24, 100, "nominal_team.png")
+matplotlib.pyplot.legend(['True Teams', 'Nominal Teams'])
+matplotlib.pyplot.savefig(pkg_resources.resource_filename("figures", "performance_v_size.png"))
+
 # TODO: Probability of winning for each nominal team size divided by team size
 # TODO: Probability of winning for each true team size divided by team size
--- kaggle_analysis/analysis.py
+++ kaggle_analysis/analysis.py
@@ -33,21 +33,27 @@ def plot_team_size_histogram(competitions: list) -> None:
     matplotlib.pyplot.savefig(pkg_resources.resource_filename("figures", "team_size_histogram.png"))
 
 
-def nominal_teams(good_competitions: list) -> None:
+def get_nominal_teams(good_competitions: list) -> tuple:
     matplotlib.pyplot.figure()
 
     real_team_scores_all = []
     indiv_team_scores_all = []
     nominal_team_scores_all = []
+
     real_team_tefforts_all = []
     indiv_team_tefforts_all = []
     nominal_team_tefforts_all = []
+
+    real_team_sizes_all = []
+    nominal_team_sizes_all = []
 
     for competition in good_competitions:
         real_team_scores = []
         indiv_team_scores = []
         real_team_tefforts = []
         indiv_team_tefforts = []
+        real_team_sizes = []
+        nominal_team_sizes = []
 
         # Check directionality
         diffscore = competition["team_list"][0]["Score"] - competition["team_list"][1]["Score"]
@@ -70,6 +76,7 @@ def nominal_teams(good_competitions: list) -> None:
             else:
                 real_team_scores.append(m*team["Score"])
                 real_team_tefforts.append(effort)
+                real_team_sizes.append(len(team["user_list"]))
 
         # Normalize values according to mean and standard distribution
         mm = numpy.mean(indiv_team_scores)
@@ -88,6 +95,7 @@ def nominal_teams(good_competitions: list) -> None:
         for _ in range(int(0.5*len(competition["team_list"]))):
             # Choose a team size
             team_size = numpy.random.choice(competition["real_team_sizes"])
+            nominal_team_sizes.append(team_size)
 
             # Get remixed average effort
             efforts = numpy.random.choice(indiv_team_tefforts, team_size, replace=False)
@@ -106,35 +114,88 @@ def nominal_teams(good_competitions: list) -> None:
         indiv_team_tefforts_all += indiv_team_tefforts
         nominal_team_tefforts_all += nominal_team_tefforts
 
-    matplotlib.pyplot.errorbar(numpy.mean(indiv_team_tefforts_all), -numpy.mean(indiv_team_scores_all),
-                               scipy.stats.sem(indiv_team_tefforts_all), scipy.stats.sem(indiv_team_scores_all), 'rs')
-    matplotlib.pyplot.errorbar(numpy.mean(real_team_tefforts_all), -numpy.mean(real_team_scores_all),
-                               scipy.stats.sem(real_team_tefforts_all), scipy.stats.sem(real_team_scores_all), 'bo')
-    matplotlib.pyplot.errorbar(numpy.mean(nominal_team_tefforts_all), -numpy.mean(nominal_team_scores_all),
-                               scipy.stats.sem(nominal_team_tefforts_all), scipy.stats.sem(nominal_team_scores_all), 'g^')
+        real_team_sizes_all += real_team_sizes
+        nominal_team_sizes_all += nominal_team_sizes
+
+    return [real_team_scores_all, real_team_tefforts_all, real_team_sizes_all], \
+           [indiv_team_scores_all, indiv_team_tefforts_all, [1]*len(indiv_team_scores_all)], \
+           [nominal_team_scores_all,  nominal_team_tefforts_all, nominal_team_sizes_all]
+
+
+def plot_nominal_teams(real_team: tuple, indiv_team: tuple, nominal_team: tuple) -> None:
+
+    matplotlib.pyplot.errorbar(numpy.mean(indiv_team[1]), -numpy.mean(indiv_team[0]),
+                               scipy.stats.sem(indiv_team[1]), scipy.stats.sem(indiv_team[0]), 'rs')
+    matplotlib.pyplot.errorbar(numpy.mean(real_team[1]), -numpy.mean(real_team[0]),
+                               scipy.stats.sem(real_team[1]), scipy.stats.sem(real_team[0]), 'bo')
+    matplotlib.pyplot.errorbar(numpy.mean(nominal_team[1]), -numpy.mean(nominal_team[0]),
+                               scipy.stats.sem(nominal_team[1]), scipy.stats.sem(nominal_team[0]), 'g^')
 
     # Add legend and labels
-    matplotlib.pyplot.legend(["Individuals (n="+str(len(indiv_team_scores_all))+")",
-                              "True Teams (n="+str(len(real_team_scores_all))+")",
-                              "Nominal Teams (n="+str(len(nominal_team_scores_all))+")"])
+    matplotlib.pyplot.legend(["Individuals (n="+str(len(indiv_team[0]))+")",
+                              "True Teams (n="+str(len(real_team[0]))+")",
+                              "Nominal Teams (n="+str(len(nominal_team[0]))+")"])
 
     delta = 0.1
     plot_slope(numpy.array([0.9, 1.1]),
-               indiv_team_tefforts_all,
-               indiv_team_scores_all, 'r:')
-    plot_slope(numpy.mean(real_team_tefforts_all)+[-delta, delta],
-               real_team_tefforts_all,
-               real_team_scores_all, 'b:')
-    plot_slope(numpy.mean(nominal_team_tefforts_all)+[-delta, delta],
-               nominal_team_tefforts_all,
-               nominal_team_scores_all, 'g:')
-    plot_slope(numpy.array([1.0, numpy.mean(nominal_team_tefforts_all)]),
-               indiv_team_tefforts_all,
-               indiv_team_scores_all, 'r:')
-    plot_slope(numpy.array([numpy.mean(real_team_tefforts_all)-delta, numpy.mean(nominal_team_tefforts_all)]),
-               real_team_tefforts_all,
-               real_team_scores_all, 'b:')
+               indiv_team[1],
+               indiv_team[0], 'r:')
+    plot_slope(numpy.mean(real_team[1])+[-delta, delta],
+               real_team[1],
+               real_team[0], 'b:')
+    plot_slope(numpy.mean(nominal_team[1])+[-delta, delta],
+               nominal_team[1],
+               nominal_team[0], 'g:')
+    plot_slope(numpy.array([1.0, numpy.mean(nominal_team[1])]),
+               indiv_team[1],
+               indiv_team[0], 'r:')
+    plot_slope(numpy.array([numpy.mean(real_team[1])-delta, numpy.mean(nominal_team[1])]),
+               real_team[1],
+               real_team[0], 'b:')
 
     matplotlib.pyplot.xlabel("Total Submissions (Normalized)")
     matplotlib.pyplot.ylabel("Quality of Best Solution (Normalized)")
     matplotlib.pyplot.savefig(pkg_resources.resource_filename("figures", "nominal_teams.png"))
+
+
+def plot_performance_v_size(team, indiv=None, file_name=None):
+    team_performance = team[0]
+    team_effort = team[1]
+    team_size = team[2]
+
+    if indiv is not None:
+        team_performance += indiv[0]
+        team_effort += indiv[1]
+        team_size += indiv[2]
+
+    team_performance = numpy.array(team_performance)
+    team_effort = numpy.array(team_effort)
+    team_size = numpy.array(team_size)
+    unique_team_sizes = numpy.unique(team_size)
+
+    data_mean = []
+    data_sem = []
+    for size in unique_team_sizes:
+        idx = numpy.where(team_size == size)[0]
+        data_mean.append(-numpy.mean(team_performance[idx]))
+        data_sem.append(scipy.stats.sem(team_performance[idx]))
+
+    matplotlib.pyplot.errorbar(unique_team_sizes, data_mean, yerr=data_sem)
+
+def plot_nominal_performance_v_size(indiv, max_team_size=25, sample_size=100, file_name=None):
+    indiv_performance = indiv[0]
+    indiv_effort = indiv[1]
+    indiv_size = indiv[2]
+
+    team_mean = []
+    team_sem = []
+    for size in range(1, max_team_size+1):
+        nominal_team_scores = []
+        for _ in range(sample_size):
+            # Get remixed scores
+            scores = numpy.random.choice(indiv_performance, size, replace=False)
+            nominal_team_scores.append(numpy.min(scores))
+        team_mean.append(-numpy.mean(nominal_team_scores))
+        team_sem.append(-scipy.stats.sem(nominal_team_scores))
+
+    matplotlib.pyplot.errorbar(range(1, max_team_size+1), team_mean, yerr=team_sem)
